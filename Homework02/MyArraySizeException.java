db29829a3c1aacb25ae4818fa533cc34d1dc572b
15STA <15sta@mail.ru>
2021-05-20 17:13:11 +0300

    Homework02 added

---
+++ Homework02/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package JavaForTesters.Lesson2.Homework02;
+
+public class MyArraySizeException extends Exception{
+    public MyArraySizeException(String message) {
+                    super(message);
+
+    }
+}
